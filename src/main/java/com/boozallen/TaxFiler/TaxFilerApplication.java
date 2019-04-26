f4bbd517d05a6425708378bfefc1696c68ef3752
Shi, John [USA] <Shi_John@bah.com>
2019-04-26 13:19:13 -0400

    Initialization
    Spring Boot Skeleton

---
+++ src/main/java/com/boozallen/TaxFiler/TaxFilerApplication.java
@@ -0,0 +1,13 @@
+package com.boozallen.TaxFiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxFilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxFilerApplication.class, args);
+	}
+
+}
