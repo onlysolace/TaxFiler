660574790e2700f39fda81eac8268121714c1550
Shi, John [USA] <Shi_John@bah.com>
2019-04-29 08:33:27 -0400

    Expanded Skeleton

---
+++ frontend/src/components/panel/TaxProficiencyPanel.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="panel">
+        <div class="row">
+            <div class="col-sm-6 col-md-4 col-lg-3">
+                <div class="item">hey</div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3">
+                <div class="item">hey2</div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3">
+                <div class="item">hey3</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "tax-proficiency-panel"
+    }
+</script>
+
+<style scoped>
+    .item{
+        height:300px;
+        width:300px;
+
+
+
+    }
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+require('bootstrap/dist/js/bootstrap')
+require('bootstrap/dist/css/bootstrap-grid.css')
+require('bootstrap/dist/css/bootstrap-reboot.css')
+require('bootstrap/dist/css/bootstrap.css')
+require('jquery')
 
 Vue.config.productionTip = false
 
